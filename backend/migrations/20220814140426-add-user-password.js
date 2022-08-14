f644b68bd709f1d6713f531f0641dd03110be671
kiqueangulo <kiqueangulo@gmail.com>
2022-08-14 10:11:01 -0400

    Add password column to user table

---
+++ backend/migrations/20220814140426-add-user-password.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.addColumn("users", "password_digest", {
+      type: Sequelize.DataTypes.STRING,
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.removeColumn("users", "password_digest");
+  },
+};
