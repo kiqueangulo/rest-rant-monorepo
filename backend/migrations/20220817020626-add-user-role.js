b82d4da762229c0d2aa3d763a2b71e5cedaaaa8f
kiqueangulo <kiqueangulo@gmail.com>
2022-08-16 22:15:28 -0400

    Add the column role to the database

---
+++ backend/migrations/20220817020626-add-user-role.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.addColumn("users", "role", {
+      type: Sequelize.DataTypes.ENUM,
+      values: ["reviewer", "admin"],
+      defaultValue: "reviewer",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.removeColumn("users", "role");
+  },
+};
